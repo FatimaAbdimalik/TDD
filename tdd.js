a6229e1bc29e218d4f6f2a659c572de581d1d85f
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-08-25 18:44:48 +0100

    first commit

---
+++ tdd.js
@@ -0,0 +1,45 @@
+function removeDuplicateWords(str) {
+  if (str === undefined) throw new Error("str is required");
+  return [...new Set(str.split(" "))].join(" ");
+}
+
+function filterTheNumber(str) {
+  if (str === undefined) throw new Error("str is required");
+  if (typeof str !== "string") throw new Error("str must be a string");
+  let regex = /[a-z]/gi;
+  return Number(str.replace(regex, ""));
+}
+function occurrencesInAnArray(array, n) {
+  if (array === undefined) throw new Error("array is required");
+  if (n === undefined) throw new Error("n is required");
+  if (typeof n !== "number") throw new Error("n must be a number");
+  let newArr = [];
+  array.forEach((p, i) => {
+    if (p === n) {
+      newArr.push(i);
+    }
+  });
+  return newArr;
+}
+function longestVowelChain(str) {
+  if (str === undefined) throw new Error("string is needed");
+  if (typeof str !== "string") throw new Error("str must be string");
+  let onlyVowels = str.replace(/[^aeiou]/gi, " ").split(" ");
+  let vowelSubStr = onlyVowels.reduce((a, b) => {
+    return a.length > b.length ? a : b;
+  }, " ");
+  return vowelSubStr.length;
+}
+function isIsogram(str) {
+  if (!str) throw new Error("string is needed");
+  return [...new Set(str.toLowerCase())].join("") === str.toLowerCase();
+}
+function orderedCountOfCharacters() {}
+module.exports = {
+  removeDuplicateWords,
+  filterTheNumber,
+  occurrencesInAnArray,
+  longestVowelChain,
+  isIsogram,
+  orderedCountOfCharacters,
+};
---
+++ tdd.test.js
@@ -0,0 +1,116 @@
+const {
+  removeDuplicateWords,
+  filterTheNumber,
+  occurrencesInAnArray,
+  longestVowelChain,
+  isIsogram,
+  orderedCountOfCharacters,
+} = require("./tdd.js");
+describe("remove duplicate words", () => {
+  test("should remove all duplicate wrods in the string", () => {
+    expect(
+      removeDuplicateWords(
+        "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+      )
+    ).toEqual("alpha beta gamma delta");
+  });
+  test("should throw an error if string is not passed", () => {
+    expect(() => {
+      removeDuplicateWords();
+    }).toThrow("str is required");
+  });
+});
+describe("return a number from a string", () => {
+  test("return numbers in the string and its original state in order", () => {
+    expect(filterTheNumber("aa1bb2cc3dd")).toBe(123);
+    expect(filterTheNumber("17feab12ff713847061a3fb6619b5530")).toBe(
+      1712713847061366195530
+    );
+  });
+  test("Throw an error if str in not passed", () => {
+    expect(() => {
+      filterTheNumber();
+    }).toThrow("str is required");
+  });
+  test("Throw an error if typefo str in not string", () => {
+    expect(() => {
+      filterTheNumber(123234345454);
+    }).toThrow("str must be a string");
+  });
+  test("Throw an error if str is not a sting", () => {
+    expect(() => {
+      filterTheNumber(false);
+    }).toThrow("str must be a string");
+  });
+});
+describe("find all occurrences of an element in an array", () => {
+  test("return another array containing all the index positions of n", () => {
+    expect(occurrencesInAnArray([6, 9, 3, 4, 3, 82, 11], 3)).toEqual([2, 4]);
+    expect(
+      occurrencesInAnArray([10, 16, 20, 6, 14, 11, 20, 2, 17, 16, 14], 16)
+    ).toEqual([1, 9]);
+    expect(
+      occurrencesInAnArray(
+        [
+          20,
+          20,
+          10,
+          13,
+          15,
+          2,
+          7,
+          2,
+          20,
+          3,
+          18,
+          2,
+          3,
+          2,
+          16,
+          10,
+          9,
+          9,
+          7,
+          5,
+          15,
+          5,
+        ],
+        20
+      )
+    ).toEqual([0, 1, 8]);
+  });
+  test("should throw an error if array is not passed", () => {
+    expect(() => {
+      occurrencesInAnArray();
+    }).toThrow("array is required");
+  });
+  test("should throw an error if n is not number", () => {
+    expect(() => {
+      occurrencesInAnArray([2, 4, 3, 2, 6, 2, 1], "2");
+    }).toThrow("n must be a number");
+  });
+  test("should throw an erro if n is not passed", () => {
+    expect(() => {
+      occurrencesInAnArray([5, 1, 2, 1, 9, 1]);
+    }).toThrow("n is required");
+  });
+});
+describe("find the length of the longest vowel substring", () => {
+  test("return the length of the longest vowel substring", () => {
+    expect(longestVowelChain("codewarriors")).toBe(2);
+    expect(longestVowelChain("iiihoovaeaaaoougjyaw")).toBe(8);
+  });
+});
+describe("return true if a string has no repeating letters", () => {
+  test("return ture if a string has no repeating letter", () => {
+    expect(isIsogram("Dermatoglyphics")).toBe(true);
+  });
+  test("return false if a string has repeating letters", () => {
+    expect(isIsogram("aba")).toBe(false);
+  });
+  test("throw an error is string is empty", () => {
+    expect(() => {
+      isIsogram("");
+    }).toThrow("string is needed");
+  });
+});
